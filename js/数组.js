dc2f6a83778668ec21e59591a69de5f9a20d0a5f
rainnosekai <849791556@qq.com>
2017-03-07 17:41:39 +0800

    2.1
    2.1

---
+++ js/数组.js
@@ -0,0 +1,22 @@
+//function grade(){
+//	this.dataStore=[];
+//	this.add=add;
+//	this.avarage=avarage;
+//}
+//function add(onegrade){
+//	this.dataStore.push(onegrade);
+//}
+//function avarage(){
+//	var total=0;
+//	for(var i=0;i<this.dataStore.length;++i){
+//		total+=this.dataStore[i]
+//	}
+//	return total/this.dataStore.length;
+//}
+//var student=new grade();
+//student.add(20);
+//student.add(30);
+//console.log(student.avarage());
+//练习2.1
+
+
